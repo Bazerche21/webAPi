cb4b484d3b2c329370bfa3a9862b686b7201575e
Bazerche21 <matejbazerkoski@gmail.com>
2024-08-18 20:31:46 +0200

    Homework #1 added

---
+++ Homework #1/Homework#1/Homework#1/StaticDb.cs
@@ -0,0 +1,15 @@
+﻿namespace Homework_1
+{
+    public static class StaticDb
+    {
+        public static List<string> UserNames = new List<string>
+        {
+            "Lazar",
+            "Stefan",
+            "Marina",
+            "Stefanija",
+            "Petar",
+            "Andrej"
+        };
+    }
+}
---
+++ Homework #1/Homework#1/Homework#1/wwwroot/simple.js
@@ -0,0 +1,12 @@
+﻿window.onload = function () {
+    fetch('/api/users')
+        .then(response => response.json())
+        .then(data => {
+            const userList = document.getElementById('userList');
+            data.forEach(user => {
+                const li = document.createElement('li');
+                li.textContent = user;
+                userList.appendChild(li);
+            });
+        });
+};
